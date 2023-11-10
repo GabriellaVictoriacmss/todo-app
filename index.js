d1b3994c7b2c9543b237a5abe2a036e42f1a64d0
Gabriella Victória <gabriellavictoria2819@gmail.com>
2023-11-09 21:20:33 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set("view engine", "handlebars")
 
-
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Olá mundo")
+    resposta.render("home")
 })
-
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
